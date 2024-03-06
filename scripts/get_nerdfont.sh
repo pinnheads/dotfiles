43452c83785fbc3a64ead32fef24c6abff75a7c5
Utsav Deep <utsavdeep01@gmail.com>
2024-03-06 23:20:55 +0530

    Add failsafe for shell commands

--- scripts/get_nerdfont.sh
+++ scripts/get_nerdfont.sh
@@ -4,21 +4,35 @@
 URL="https://github.com/ryanoasis/nerd-fonts/releases/download/v3.1.1/JetBrainsMono.zip"
 DIR="$HOME/dotfiles/.fonts"
 
+# Create the directory if it doesn't exist
+mkdir -p "$DIR"
+
 # Check if curl is installed or not
 if ! command -v curl &> /dev/null
 then
-	echo "curl not found"
+	echo "curl not found!! Install with apt install curl"
 	exit
 fi
-
-# Create the directory if it doesn't exist
-mkdir -p "$DIR"
 
 # Download the file to the target directory
 curl -L "$URL" -o "$DIR/JetBrainsMono.zip"
 
+# Check if curl is installed or not
+if ! command -v unzip &> /dev/null
+then
+	echo "unzip not found!! Install with apt install unzip"
+	exit
+fi
+
 # Unzip the file
 unzip "$DIR/JetBrainsMono.zip" -d "$DIR"
+
+# Check if curl is installed or not
+if ! command -v fc-cache &> /dev/null
+then
+	echo "fontconfig not found!! Install with apt install fontconfig"
+	exit
+fi
 
 # run fc-cache -v
 fc-cache -v
