8446aa74c67090c6f39690165446981940f654d0
Utsav Deep <utsavdeep01@gmail.com>
2024-09-04 16:14:15 +0200

    Add tmux config and cheat sheet script

---
+++ bin/.local/scripts/cht.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+languages=$(echo "golang java c cpp python javascript typescript rust html css sass" | tr " " "\n")
+core_utils=$(echo "find xargs awk sed grep vim mkdir tmux" | tr " " "\n")
+selected=$(echo "$languages\n$core_utils" | fzf)
+
+read -p "Enter search query: " query
+
+if echo "$languages" | grep -q "$selected"; then
+    tmux split-window -p 40 -h bash -c "curl -s cht.sh/$selected/$(echo $query | tr " " "+") | less"
+else
+    tmux split-window -p 40 -h bash -c "curl -s cht.sh/$selected~$query | less"
+fi
