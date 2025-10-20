dbe49d3f127fff75686003594140418dbe2b2c5a
Utsav Deep <utsavdeep01@gmail.com>
2025-10-20 23:51:32 +0200

    Update monitor handling

---
+++ hypr/.config/hypr/handle_monitor_connect.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+## Taken from https://wiki.hypr.land/FAQ/
+
+handle() {
+  case $1 in monitoradded*)
+    hyprctl dispatch moveworkspacetomonitor "1 1"
+    hyprctl dispatch moveworkspacetomonitor "2 1"
+    hyprctl dispatch moveworkspacetomonitor "5 0"
+  esac
+}
+
+socat - "UNIX-CONNECT:$XDG_RUNTIME_DIR/hypr/${HYPRLAND_INSTANCE_SIGNATURE}/.socket2.sock" | while read -r line; do handle "$line"; done
